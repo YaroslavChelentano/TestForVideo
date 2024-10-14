532130923ba41c9cb77a7262e637ba9bf7792049
Yaroslav Andreiev <49638499+YaroslavChelentano@users.noreply.github.com>
2024-10-14 15:26:27 +0300

    Update Program.cs

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello repo");
+            Console.WriteLine("Hello repo edited from web");
         }
     }
 }
