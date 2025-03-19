5e60ed989c3aa686469f37507bdcf0e193de9a39
Yaroslav Andreiev <yaroslav.andreiev@devsx.net>
2025-03-19 12:38:35 +0200

    SFS-8 delete hello world

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -4,8 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello");
             Console.WriteLine("Hello repo edited from web");
         }
     }
